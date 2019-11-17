13727e614e420fc0e895f40591510c425effd3ff
Thong Nguyen <thong@orangetheoryfitness.com>
2019-11-18 02:10:18 +0700

    Create UI Login - add material design to project

---
+++ 2PrintingManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _2PrintingManagement.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool isLoaded = false;
+        // Xử lý mọi thứ trong này 
+        public MainViewModel()
+        {
+            if (!isLoaded)
+            {
+                isLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
