92daa021287b35cd6b520ea93498ff0f735a0303
Thong Nguyen <nguyenmanhthong17596@gmail.com>
2019-11-19 02:01:20 +0700

    - Create Control Bar viewmodel - create control bar view - Get Window parent

---
+++ 2PrintingManagement/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace _2PrintingManagement.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand  CloseWindowCommand { get; set; }
+        #endregion
+
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p)=> { return p == null? false : true; },(p)=> { GetWindowParents(p); });
+        }
+
+        void GetWindowParents(UserControl p )
+        {
+            FrameworkElement t = p.Parent as FrameworkElement;
+
+            FrameworkElement tt = t.Parent as FrameworkElement;
+        }
+    }
+}
+  
